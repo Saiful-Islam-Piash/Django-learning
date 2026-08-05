89a09fb16f79944dfc5023825fd107bbcea90598
Saiful Islam Piash <saiful3217islam@gmail.com>
2026-08-05 23:51:45 +0200

    more update

--- authentication_and_authorization/firstapp/forms.py
+++ authentication_and_authorization/firstapp/forms.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class RegisterForm(UserCreationForm):
-
-    def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
-    
-                for field in self.fields.values():
-                    field.help_text = None
-
+    first_name=forms.CharField(widget=forms.TextInput(attrs={'id':'required'}))
     class Meta:
+        
         model= User
         fields=['username','first_name','last_name','email']
 
-        
+       
--- authentication_and_authorization/firstapp/views.py
+++ authentication_and_authorization/firstapp/views.py
@@ -3,5 +3,10 @@ from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
-    form=RegisterForm()
+    if request.method =='POST':
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+    else:
+        form=RegisterForm()
     return render(request,'home.html',{'form':form})
